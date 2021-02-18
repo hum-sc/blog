638dd4bd65461a11368c62b266daf4d76bc6da3c
hum-sc <humbertoantsc@gmail.com>
2021-02-17 20:19:26 -0600

    contenedor menu

---
+++ src/components/menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/menu.css'
+
+function Menu (props){
+    return (
+        <div className='container'>
+
+        </div>
+    );
+}
+
+export default Menu;
